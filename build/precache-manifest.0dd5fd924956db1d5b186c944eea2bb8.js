cdca1667a7acaea004399252287d18001c1e51fc
ravishan123 <66440787+ravishan123@users.noreply.github.com>
2021-06-20 01:41:10 +0530

    dsdsddf

---
+++ build/precache-manifest.0dd5fd924956db1d5b186c944eea2bb8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9184b1786fc3e98348fcff375b6434f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2015e075e8e840fd75f",
+    "url": "/static/css/main.fac6225b.chunk.css"
+  },
+  {
+    "revision": "c269d724f5ea4ddae7a1",
+    "url": "/static/js/2.33d9d61c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.33d9d61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2015e075e8e840fd75f",
+    "url": "/static/js/main.dc6687d1.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "01162f57ac7598dceeda80a637413921",
+    "url": "/static/media/NewsbyCategories.01162f57.jpg"
+  },
+  {
+    "revision": "3c54ebfd89bf40062c8c241b718a294f",
+    "url": "/static/media/NewsbySources.3c54ebfd.jpg"
+  },
+  {
+    "revision": "867f52c8594a7000afa6c8908bbea2d8",
+    "url": "/static/media/NewsbyTerms.867f52c8.jpg"
+  },
+  {
+    "revision": "9f61dbbec411177ee5c5cffb0f051128",
+    "url": "/static/media/background.9f61dbbe.svg"
+  },
+  {
+    "revision": "74d4c03302e31d1f5cab0e2ae59a003d",
+    "url": "/static/media/latestnews.74d4c033.jpg"
+  },
+  {
+    "revision": "1682ad84d09850918cd9ac62051e03df",
+    "url": "/static/media/logo.1682ad84.png"
+  }
+]);
